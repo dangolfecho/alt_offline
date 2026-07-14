5930370ccb0b1bf643be2128e8030e352c091bcb
gideon <55153800+dangolfecho@users.noreply.github.com>
2026-07-14 15:44:06 +0530

    Wrote code to create datasets from a trained policy

--- collect.py
+++ collect.py
@@ -9,11 +9,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 from gymnasium import spaces
 from rl_zoo3.train import train
-from stable_baselines3 import PPO
+from stable_baselines3 import A2C, DDPG, DQN, SAC, TD3, PPO
 from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
 
-import minari
 from minari import DataCollector
 import PyFlyt.gym_envs
 
@@ -29,30 +28,60 @@ envs = ["PyFlyt/QuadX-Hover-v4", "PyFlyt/QuadX-Pole-Balance-v4",
         "PyFlyt/QuadX-Waypoints-v4", "PyFlyt/Fixedwing-Waypoints-v3", "PyFlyt/Rocket-Landing-v4"]
 
 #Dataset creation
-def policy(env, algo_num):
-    #using uniformly random policy
-    if(algo_num == 0):
-        return env.action_space.sample()
+algos = ['a2c', 'ddpg', 'sac', 'td3', 'ppo']
 
 algorithm_map = {
         0: 'random',
+        10: algos[0],
+        11: algos[1],
+        12: algos[2], 
+        13: algos[3],
+        14: algos[4],
         }
 
+def get_model(env, env_str, algorithm_str):
+    pack_name, env_name = env_str.split('/')
+    #model_path = f'../alt_drones/{env_name}/{algorithm_str}.zip'
+    model_path = f'../alt_drones/backup/{algorithm_str}{env_name}.zip'
+    if(algorithm_str == 'a2c'):
+        return A2C.load(model_path, env)
+    elif(algorithm_str == 'ddpg'):
+        return DDPG.load(model_path, env)
+    elif(algorithm_str == 'sac'):
+        return SAC.load(model_path, env)
+    elif(algorithm_str == 'td3'):
+        return TD3.load(model_path, env)
+    elif(algorithm_str == 'ppo'):
+        return PPO.load(model_path, env)
+
+def policy_gen(env, algo_num, obs, model=None):
+    if(algo_num == 0):
+        return env.action_space.sample()
+    elif(algo_num >= 10 and algo_num <= 14):
+        return model.predict(obs)
+
 def main(env_num=DEFAULT_ENV, algo_num=DEFAULT_ALGO):
-    env = minari.DataCollector(gym.make(envs[env_num]))
+    env = gym.make(envs[env_num])
+    collector_env = DataCollector(env)
 
-    pack_name, actual_env_name = envs[env_num].split('/')
+    obs, _ = collector_env.reset()
 
-    env.reset()
+    if(algo_num == 0):
+        model = None
+    else:
+        model = get_model(env, envs[env_num], algorithm_map[algo_num])
 
     for _ in range(NUM_SAMPLES):
-        action = policy(env, algo_num)
-        obs, rew, terminated, truncated, info = env.step(action)
+        if(algo_num == 0):
+            action = policy_gen(env, algo_num, obs, model)
+        else:
+            action, states = policy_gen(env, algo_num, obs, model)
+        obs, rew, terminated, truncated, info = collector_env.step(action)
 
         if terminated or truncated:
-            env.reset()
-
-    dataset = env.create_dataset(dataset_id=f'{actual_env_name}/dataset-v{algo_num}',
+            obs, _ = collector_env.reset()
+    _, actual_env_name = (envs[env_num]).split('/')
+    dataset = collector_env.create_dataset(dataset_id=f'{actual_env_name}/dataset-v{algo_num}',
             eval_env=None,
             author='Gideon',
             author_email='filler_email',
