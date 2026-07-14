7364a41928e40cdbadf7ee9b82a6916e6752a960
gideon <55153800+dangolfecho@users.noreply.github.com>
2026-07-14 15:44:46 +0530

    Some debugging changes

--- d3_code.py
+++ d3_code.py
@@ -9,14 +9,14 @@ cql = d3rlpy.algos.DiscreteCQLConfig().create()
 
 cql.fit(
         dataset,
-        n_steps=1000,
+        n_steps=10,
         n_steps_per_epoch=100,
         evaluators={"environment": d3rlpy.metrics.EnvironmentEvaluator(env)}
 )
 
 d3rlpy.notebook_utils.start_virtual_display()
 
-env = RecordVideo(gym.make("D4RL/door/human-v2", render_mode="rgb_array"),
+env = RecordVideo(gym.make("cartpole/expert-v0", render_mode="rgb_array"),
         './video')
 
 d3rlpy.metrics.evaluate_qlearning_with_environment(cql, env)
--- run_script.sh
+++ run_script.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 python collect.py "0" "0"
+python collect.py "0" "10"
+python collect.py "0" "11"
+python collect.py "0" "12"
+python collect.py "0" "13"
+python collect.py "0" "14"
