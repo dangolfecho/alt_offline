a2d5f67fdcae402691c3cc63e16592343d131016
gideon <55153800+dangolfecho@users.noreply.github.com>
2026-07-20 14:58:12 +0530

    Misc changes to scripts

--- record_script.sh
+++ record_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 
-d3rlpy record d3rlpy_logs/SAC_QuadX-Hover-v4_0_20260716153033/model_100000.d3 \
+d3rlpy record d3rlpy_logs/SAC_QuadX-Hover-v4_0_20260718142220/model_2000000.d3 \
 --env-header 'import PyFlyt.gym_envs; import gymnasium as gym; \
 env=gym.make("PyFlyt/QuadX-Hover-v4", render_mode="human")' --out .
--- run_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python collect.py "0" "0"
-python collect.py "0" "10"
-python collect.py "0" "11"
-python collect.py "0" "12"
-python collect.py "0" "13"
-python collect.py "0" "14"
