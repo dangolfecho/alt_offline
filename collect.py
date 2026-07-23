c307d64888aa21e8233e85ae3d1ffea8eee02438
gideon <55153800+dangolfecho@users.noreply.github.com>
2026-07-23 18:36:28 +0530

    Modified code to allow collecting datasets of environments with waypoints

--- collect.py
+++ collect.py
@@ -16,14 +16,27 @@ from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
 
 from minari import DataCollector
+from minari.serialization import deserialize_space, serialize_space
 import PyFlyt.gym_envs
+from gymnasium.wrappers import FlattenObservation
+
+from gymnasium.spaces import Box, Sequence
+from typing import Dict, Union
 
 from tqdm import tqdm
+import warnings
+
+from PyFlyt.gym_envs import FlattenWaypointEnv
+
+
+from gymnasium.envs.registration import EnvSpec
+
+warnings.filterwarnings('ignore')
 
 
 DEFAULT_ENV = 0
 DEFAULT_ALGO = 0
-NUM_SAMPLES = int(1e6)
+NUM_SAMPLES = int(1e3)
 
 torch.manual_seed(42)
 
@@ -43,10 +56,25 @@ algorithm_map = {
         14: algos[4],
         }
 
+env_config = {
+        'render_mode': 'rgb_array',
+}
+
+
+def get_env(env_str):
+    if(env_str in (envs[3], envs[4], envs[5])):
+        env = gym.make(env_str)
+        context_length = env_config.get('context_length', 4)
+        env = FlattenWaypointEnv(env, context_length)
+        return env
+    else:
+        return gym.make(env_str)
+    
 def get_model(env, env_str, algorithm_str):
     pack_name, env_name = env_str.split('/')
     #model_path = f'../alt_drones/{env_name}/{algorithm_str}.zip'
-    model_path = f'../alt_drones/backup/{algorithm_str}{env_name}.zip'
+    #model_path = f'../alt_drones/backup/{algorithm_str}{env_name}.zip'
+    model_path = f'../alt_drones/results/{env_name}/{algorithm_str}.zip'
     if(algorithm_str == 'a2c'):
         return A2C.load(model_path, env)
     elif(algorithm_str == 'ddpg'):
@@ -64,10 +92,27 @@ def policy_gen(env, algo_num, obs, model=None):
     elif(algo_num >= 10 and algo_num <= 14):
         return model.predict(obs)
 
+#If sequence contains box
+#Sequence(Box(-x, x, (3,), float64), stack=True)
+@serialize_space.register(spaces.Sequence)
+def serialize_sequence(space: spaces.Sequence, to_string=True) -> Union[Dict, str]:
+    box_obj = ((space.__dict__)['feature_space'])
+    result = {}
+    result["type"] = "Sequence"
+    result["subspaces"] = serialize_space(box_obj)
+
+    if to_string:
+        result = json.dumps(result)
+    return result
+
+@deserialize_space.register("Sequence")
+def deserialize_sequence(space_dict: Dict) -> spaces.Sequence:
+    assert space_dict["type"] == "Sequence"
+    box_obj = deserialize_space(space_dict['subspaces'])
+    return spaces.Sequence(box_obj, stack=True)
+
 def main(env_num=DEFAULT_ENV, algo_num=DEFAULT_ALGO):
-    env = make_vec_env(envs[env_num], n_envs=16, vec_env_cls=SubprocVecEnv,
-            env_kwargs={'render_mode': 'rgb_array'},
-            vec_env_kwargs=dict(start_method='fork'),)
+    env = get_env(envs[env_num])
     collector_env = DataCollector(env)
 
     obs, _ = collector_env.reset()
@@ -88,10 +133,10 @@ def main(env_num=DEFAULT_ENV, algo_num=DEFAULT_ALGO):
             obs, _ = collector_env.reset()
     _, actual_env_name = (envs[env_num]).split('/')
     dataset = collector_env.create_dataset(dataset_id=f'{actual_env_name}/dataset-v{algo_num}',
-            eval_env=None,
+            eval_env=envs[env_num],
+            algorithm_name=algorithm_map[algo_num],
             author='Gideon',
             author_email='filler_email',
-            algorithm_name=algorithm_map[algo_num],
             code_permalink='https://github.com/dangolfecho/alt_offline',
             )
     env.close()
--- train.py
+++ train.py
@@ -4,6 +4,15 @@ import gymnasium as gym
 import PyFlyt.gym_envs
 import torch
 import os
+import minari
+from minari import DataCollector
+from gymnasium import spaces
+from minari.serialization import deserialize_space, serialize_space
+from gymnasium.spaces import Box, Sequence
+from typing import Dict, Union
+
+
+from PyFlyt.gym_envs import FlattenWaypointEnv
 
 #os.environ['RANK'] = '0'
 #os.environ['WORLD_SIZE'] = '4'
@@ -14,16 +23,34 @@ envs = ["PyFlyt/QuadX-Hover-v4", "PyFlyt/QuadX-Pole-Balance-v4",
         "PyFlyt/QuadX-Ball-In-Cup-v4", "PyFlyt/QuadX-Pole-Waypoints-v4",
         "PyFlyt/QuadX-Waypoints-v4", "PyFlyt/Fixedwing-Waypoints-v3", "PyFlyt/Rocket-Landing-v4"]
 
-def main():
+@serialize_space.register(spaces.Sequence)
+def serialize_sequence(space: spaces.Sequence, to_string=True) -> Union[Dict, str]:
+    box_obj = ((space.__dict__)['feature_space'])
+    result = {}
+    result["type"] = "Sequence"
+    result["subspaces"] = serialize_space(box_obj)
+
+    if to_string:
+        result = json.dumps(result)
+    return result
+
+@deserialize_space.register("Sequence")
+def deserialize_sequence(space_dict: Dict) -> spaces.Sequence:
+    assert space_dict["type"] == "Sequence"
+    box_obj = deserialize_space(space_dict['subspaces'])
+    return spaces.Sequence(box_obj, stack=True)
+
+DEFAULT_ENV = 0
+DEFAULT_DATASET = 0
+def main(env_num=DEFAULT_ENV, dataset_num=DEFAULT_DATASET):
 
     rank = d3rlpy.distributed.init_process_group("gloo")
     print(f"Start running on rank={rank}")
 
     device = 'cpu:0'
 
-
-    pack_name, ac_name = envs[0].split('/')
-    dataset, env = d3rlpy.datasets.get_minari(f'{ac_name}/dataset-v0')
+    pack_name, ac_name = envs[env_num].split('/')
+    dataset, env = d3rlpy.datasets.get_minari(f'{ac_name}/dataset-v{dataset_num}')
 
     d3rlpy.seed(0)
     d3rlpy.envs.seed_env(env, 0)
@@ -38,7 +65,12 @@ def main():
     logger_adapter: d3rlpy.logging.LoggerAdapterFactory
     evaluators: dict[str, d3rlpy.metrics.EvaluatorProtocol]
     if rank == 0:
-        evaluators = {'environment': d3rlpy.metrics.EnvironmentEvaluator(env)}
+        if(env_num >=3 and env_num <=5):
+            evaluators = {'environment':
+                    d3rlpy.metrics.EnvironmentEvaluator(FlattenWaypointEnv(env,4))}
+        else:
+            evaluators = {'environment':
+                    d3rlpy.metrics.EnvironmentEvaluator(env)}
         logger_adapter = d3rlpy.logging.FileAdapterFactory()
     else:
         evaluators = {}
@@ -46,22 +78,32 @@ def main():
 
     #cql.fit(dataset,
     sac.fit(dataset,
-            n_steps=int(2e6),
+            n_steps=int(1e3),
+            #n_steps=int(2e6),
             n_steps_per_epoch=1000,
             save_interval=10,
             logger_adapter=logger_adapter,
-            evaluators={'environment':
-                d3rlpy.metrics.EnvironmentEvaluator(env)},
-            experiment_name=f'SAC_{ac_name}_{0}',
+            evaluators= evaluators,
+            experiment_name=f'SAC_{ac_name}_{dataset_num}',
             show_progress=rank == 0,
     )
 
-    sac.save_model(f'SAC_{ac_name}_final.pt')
+    sac.save_model(f'SAC_{ac_name}_{dataset_num}_final.pt')
 
     d3rlpy.distributed.destroy_process_group()
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+            prog='train.py',
+            description='does offline training',
+            )
+    parser.add_argument('env_num', type=int, default=DEFAULT_ENV, help='which\
+            environment to train onfrom')
+    parser.add_argument('dataset_num', type=int, default=DEFAULT_DATASET, help='which\
+            algorithm to use for collecting data')
+    ARGS = parser.parse_args()
+    main(**vars(ARGS))
+
 
 '''
 Need to do these changes in d3rlpy for this to work
@@ -93,4 +135,49 @@ def get_minari(
         else:
             _dataset = minari.load_dataset(env_name, download=False)
 
+'''
+'''
+gotta make this change in
+also add import gymnasium at top
+~/miniconda/envs/d4rl/lib/python3.10/site-packages/PyFlyt/gym_envs/utils/flatten_waypoint_env.py
+class FlattenWaypointEnv(ObservationWrapper,
+        gymnasium.utils.RecordConstructorArgs):
+    """FlattenWaypontEnv."""
+
+    def __init__(self, env: Env, context_length=2):
+        """__init__.
+
+        Args:
+            env (Env): a PyFlyt Waypoints environment.
+            context_length: how many waypoints should be included in the flattened observation space.
+
+        """
+        super().__init__(env=env)
+        gymnasium.utils.RecordConstructorArgs.__init__(self,
+                context_length=context_length)
+        if not hasattr(env, "waypoints") and not isinstance(
+            env.unwrapped.waypoints,  # type: ignore[reportAttributeAccess]
+            WaypointHandler,
+        ):
+            raise AttributeError(
+                "Only a waypoints environment can be used with the `FlattenWaypointEnv` wrapper."
+            )
+        self.context_length = context_length
+        self.attitude_shape = env.observation_space["attitude"].shape[0]  # type: ignore [reportGeneralTypeIssues]
+        self.target_shape = env.observation_space["target_deltas"].feature_space.shape[  # type: ignore [reportGeneralTypeIssues]
+            0
+        ]  # type: ignore [reportGeneralTypeIssues]
+        self.observation_space = Box(
+            low=-np.inf,
+            high=np.inf,
+            shape=(self.attitude_shape + self.target_shape * self.context_length,),
+        )
+
+    def observation(self, observation) -> np.ndarray:
+        """Flattens an observation from the super env.
+
+        Args:
+            observation: a dictionary observation with an "attitude" and "target_deltas" keys.
+
+        """
 '''
