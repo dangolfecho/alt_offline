9a5a5e76bce99981225597081a20f1c59ee97804
gideon <55153800+dangolfecho@users.noreply.github.com>
2026-08-05 11:31:25 +0530

    more bugs

---
+++ eval.py
@@ -0,0 +1,54 @@
+import d3rlpy
+import argparse
+import onnxscript
+import gymnasium as gym
+import PyFlyt.gym_envs
+import pandas as pd
+import numpy as np
+
+DEFAULT_NAME = "0"
+DEFAULT_NUM = int(1e5)
+
+envs = ["PyFlyt/QuadX-Hover-v4", "PyFlyt/QuadX-Pole-Balance-v4",
+        "PyFlyt/QuadX-Ball-In-Cup-v4", "PyFlyt/QuadX-Pole-Waypoints-v4",
+        "PyFlyt/QuadX-Waypoints-v4", "PyFlyt/Fixedwing-Waypoints-v3", "PyFlyt/Rocket-Landing-v4"]
+
+def main(mname=DEFAULT_NAME):
+    iter_num = DEFAULT_NUM
+    save_path = f'backup/plot_cache/{mname}/model_' 
+    env = gym.make(envs[0])
+    model = d3rlpy.load_learnable(f'{save_path}{iter_num}.d3')
+
+    ep_count = 0
+    observation = np.random.random((1, 3))
+    observation = env.reset()
+    rewards = [[]]
+    while ep_count < 10:
+        obs = observation[0][:, None]
+        action = model.predict(obs)[0]
+        observation, reward, done, _ = env.step(action)
+        rewards[ep_count].append(reward)
+        if done:
+            observation = env.reset()
+            ep_count += 1
+            rewards.append([])
+
+    df = pd.DataFrame(reward)
+    df.to_csv("rewards.csv")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+            prog='eval.py',
+            description='evaluates trained models')
+    parser.add_argument('mname', type=str, default=DEFAULT_NAME, help='name of\
+        file which contains models')
+    ARGS = parser.parse_args()
+    main(**vars(ARGS))
+
+'''
+modify gym_envs/quadx_envs/quadx_hover_env.py to take start position and
+orientation as arguments
+
+
+'''
---
+++ evalscript.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python eval.py "SAC_QuadX-Hover-v4_10_20260720192943"
+#python eval.py "Finetune_SAC_QuadX-Hover-v4_10_SAC_20260721160356"
