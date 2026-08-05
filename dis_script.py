1712644b79b01326945e4bb09cdf647433c0b5c5
gideon <55153800+dangolfecho@users.noreply.github.com>
2026-08-05 11:32:41 +0530

    Trivial change in script

--- dis_script.py
+++ dis_script.py
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-python -m torch.distributed.run train.py "3" "0"
+python -m torch.distributed.run train.py "0" "11"
+python -m torch.distributed.run train.py "0" "12"
+python -m torch.distributed.run train.py "0" "13"
+python -m torch.distributed.run train.py "0" "14"
